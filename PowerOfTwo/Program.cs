fe07c557ed1bb1511408d11ab5dfa3cf0237d3d5
Stepanov Vadim <vadgat777@mail.ru>
2022-07-01 18:16:55 +0700

    second commit

--- PowerOfTwo/Program.cs
+++ PowerOfTwo/Program.cs
@@ -9,7 +9,7 @@ namespace PowerOfTwo
             Console.WriteLine(" Программа степень двойки:");
 
             Random random = new Random();
-            int two = 2;
+            int baseDegree = 2;
             int initialNumber = 1;
             int finitNumber = 100;
             int result = 0;
@@ -19,7 +19,7 @@ namespace PowerOfTwo
 
             for (minDegree = 0; result < number; minDegree++)
             {
-                result = (int)Math.Pow(two, minDegree);
+                result = (int)Math.Pow(baseDegree, minDegree);
             }
 
             Console.WriteLine("\n Вывод числа N = " + number + ", степень minDegree = " + minDegree + " и само число 2 в найденной степени result = " + result + ".");
